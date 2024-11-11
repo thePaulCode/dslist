1e486f55d477503966719eb9554d7ba505b6fe20
Paulo Santos <santospaulor21@gmail.com>
2024-11-11 11:53:33 -0300

    Projeto criado

---
+++ src/test/java/com/thepaulcode/dslist/IntensivoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepaulcode.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
